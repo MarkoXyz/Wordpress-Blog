670fcdc3cbcb1ee616d83d7271f0569a582eeb26
MarkoXyz <704712641@qq.com>
2020-08-09 20:04:23 +0800

    update js

--- wp-content/themes/kratos-pjax/static/js/kratos.js
+++ wp-content/themes/kratos-pjax/static/js/kratos.js
@@ -371,21 +371,7 @@ function createtime(){
     now.setTime(now.getTime()+250);
     days = (now-grt)/1000/60/60/24;dnum = Math.floor(days);
     document.getElementById('span_dt_dt').innerHTML = dnum+'天';
-    // hours = (now-grt)/1000/60/60-(24*dnum);hnum = Math.floor(hours);
-    // if(String(hnum).length==1){hnum = '0'+hnum;}
-    // minutes = (now-grt)/1000/60-(24*60*dnum)-(60*hnum);mnum = Math.floor(minutes);
-    // if(String(mnum).length==1){mnum = '0'+mnum;}
-    // seconds = (now-grt)/1000-(24*60*60*dnum)-(60*60*hnum)-(60*mnum);snum = Math.round(seconds);
-    // if(String(snum).length==1){snum = '0'+snum;}
-    // document.getElementById('span_dt_dt').innerHTML = dnum+'天'+hnum+'小时'+mnum+'分'+snum+'秒';
 }
-// setInterval('createtime()',250);
 createtime()
 //copy
-if(xb.copy) document.body.oncopy=function(){alert('已复制所选内容，请务必遵守本站条约！');}
-//console
-window.onload = function(){
-    var now = new Date().getTime();
-    var page_load_time = now-performance.timing.navigationStart;
-    console.log('页面加载完毕消耗时间：'+Math.round(performance.now()*100)/100+'ms');
-};
+if(xb.copy) document.body.oncopy=function(){alert('已复制所选内容，请务必遵守知识分享条款！');}
