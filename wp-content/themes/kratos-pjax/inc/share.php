d9d0831d3b9e9ce9ac22c654c042c5ebb9cb4673
MarkoXyz <704712641@qq.com>
2020-08-10 22:37:10 +0800

    update api

--- wp-content/themes/kratos-pjax/inc/share.php
+++ wp-content/themes/kratos-pjax/inc/share.php
@@ -25,8 +25,8 @@
                 <i class="fa fa-weixin"></i>
             </div>
             <div class="share-int">
-                <!--<div class="qrcode"><img src="https://api.fczbl.vip/qr/?m=0&url=<?php the_permalink() ?>" width="150" height="150"></div>-->
-                <div class="qrcode"><img src="https:///www.markoxu.com/wp-content/themes/kratos-pjax/static/images/default-nothing.png" width="150" height="150"></div>
+                <div class="qrcode"><img src="https://api.fczbl.vip/qr/?m=0&url=<?php the_permalink() ?>" width="150" height="150"></div>
+                <!--<div class="qrcode"><img src="https:///www.markoxu.com/wp-content/themes/kratos-pjax/static/images/default-nothing.png" width="150" height="150"></div>-->
                 <p><?php _e('扫一扫，分享吧~','moedog'); ?></p>
             </div>
         </a>
